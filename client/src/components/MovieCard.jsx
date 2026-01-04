7863cce9836f7e3a33f8f3dcf143d3d79528f066
Inayat Ilkal <inayatilkal@gmail.com>
2026-01-04 12:20:39 +0530

    Add minimum price display to MovieCard
    Updated MovieCard component to include minimum price display and adjusted styling.

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -5,6 +5,7 @@ import timeFormat from '../lib/timeFormat'
 import { useAppContext } from '../context/AppContext'
 import { Heart } from 'lucide-react'
 import toast from 'react-hot-toast'
+import { currencyFormat } from '../lib/currencyFormat'
 
 const MovieCard = ({ movie }) => {
 
@@ -38,7 +39,7 @@ const MovieCard = ({ movie }) => {
   }
 
   return (
-    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66'>
+    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-70'>
 
       <img onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }}
         src={image_base_url + movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer' />
@@ -49,7 +50,13 @@ const MovieCard = ({ movie }) => {
         {new Date(movie.release_date).getFullYear()} • {movie.genres.slice(0, 2).map(genre => genre.name).join(" | ")} • {timeFormat(movie.runtime)}
       </p>
 
-      <div className='flex items-center justify-between mt-4 pb-3'>
+       {movie.minPrice && (
+        <p className='flex items-center gap-1 text-sm text-gray-300 font-medium pt-1'>
+          {currencyFormat(movie.minPrice)}
+        </p>
+      )}
+
+      <div className='flex items-center justify-between mt-2 pb-3'>
         <div className="flex items-center gap-2">
           <button onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Buy Tickets</button>
           <button onClick={handleFavorite} className='px-3 py-2 text-xs bg-gray-700 hover:bg-gray-600 transition rounded-full font-medium flex items-center gap-2'>
@@ -58,7 +65,7 @@ const MovieCard = ({ movie }) => {
           </button>
         </div>
 
-        <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
+        <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1 ml-1'>
           <StarIcon className="w-4 h-4 text-primary fill-primary" />
           {movie.vote_average.toFixed(1)}
         </p>

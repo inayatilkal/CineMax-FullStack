24830f8838ac637ceaf2ac5e9a8ab7c992637a19
Inayat Ilkal <inayatilkal@gmail.com>
2026-01-10 12:56:22 +0530

    Add paymentSessionId field to Booking model

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -7,6 +7,7 @@ const bookingSchema = new mongoose.Schema({
     bookedSeats: { type: Array, required: true },
     isPaid: { type: Boolean, default: false },
     paymentLink: { type: String },
+    paymentSessionId: { type: String },
 }, { timestamps: true })
 
 const Booking = mongoose.model("Booking", bookingSchema);

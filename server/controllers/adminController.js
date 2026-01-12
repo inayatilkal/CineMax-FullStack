c5345d70ac9ce25d015fbd62f4cc1e0fb0b89001
Inayat Ilkal <inayatilkal@gmail.com>
2026-01-12 16:01:12 +0530

    Format adminController.js for consistency

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -4,53 +4,56 @@ import User from "../models/User.js";
 
 
 // API to check if user is admin
-export const isAdmin = async (req, res) =>{
-    res.json({success: true, isAdmin: true})
+export const isAdmin = async (req, res) => {
+    res.json({ success: true, isAdmin: true })
 }
 
 // API to get dashboard data
-export const getDashboardData = async (req, res) =>{
+export const getDashboardData = async (req, res) => {
     try {
-        const bookings = await Booking.find({isPaid: true});
-        const activeShows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie');
+        const bookings = await Booking.find({ isPaid: true });
+        const activeShows = await Show.find({ showDateTime: { $gte: new Date() } }).populate('movie');
 
         const totalUser = await User.countDocuments();
 
         const dashboardData = {
             totalBookings: bookings.length,
-            totalRevenue: bookings.reduce((acc, booking)=> acc + booking.amount, 0),
+            totalRevenue: bookings.reduce((acc, booking) => acc + booking.amount, 0),
             activeShows,
             totalUser
         }
 
-        res.json({success: true, dashboardData})
+        res.json({ success: true, dashboardData })
     } catch (error) {
         console.error(error);
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // API to get all shows
-export const getAllShows = async (req, res) =>{
+export const getAllShows = async (req, res) => {
     try {
-        const shows = await Show.find({showDateTime: { $gte: new Date() }}).populate('movie').sort({ showDateTime: 1 })
-        res.json({success: true, shows})
+        const shows = await Show.find({ showDateTime: { $gte: new Date() } }).populate('movie').sort({ showDateTime: 1 })
+        res.json({ success: true, shows })
     } catch (error) {
         console.error(error);
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // API to get all bookings
-export const getAllBookings = async (req, res) =>{
+export const getAllBookings = async (req, res) => {
     try {
-        const bookings = await Booking.find({}).populate('user').populate({
+        const bookings = await Booking.find({}).populate({ path: 'user', model: 'User' }).populate({
             path: "show",
-            populate: {path: "movie"}
+            populate: { path: "movie" }
         }).sort({ createdAt: -1 })
-        res.json({success: true, bookings })
+
+
+
+        res.json({ success: true, bookings })
     } catch (error) {
         console.error(error);
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
